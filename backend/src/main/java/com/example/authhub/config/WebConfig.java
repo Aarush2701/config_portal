ce65faa8ba08a919281bb3ff7590843f86ef00ab
Aarush2701 <aarushg501@gmail.com>
2025-07-13 23:06:04 +0530

    cors

--- backend/src/main/java/com/example/authhub/config/WebConfig.java
+++ backend/src/main/java/com/example/authhub/config/WebConfig.java
@@ -1,17 +1,17 @@
 package com.example.authhub.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:4200") // Allow Angular frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+        registry.addMapping("/**")
+                    .allowedOrigins("https://config-portal-1.onrender.com") // your frontend URL
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
     }
 }
